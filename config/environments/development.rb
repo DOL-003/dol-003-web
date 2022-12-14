e9eb8d40c441cc6a140fda970006fd88edcb9e91
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-13 22:15:28 -0500

    flash updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,13 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.default_url_options = {
+    host: 'localhost:3199'
+  }
+
+  config.action_mailer.smtp_settings = {
+    :address => "mailcatcher",
+    :port => 1025
+  }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
 
   get '/about', to: 'application#about'
 
-  get '/sign-in', to: 'sessions#sign_in', as: 'sign_in'
-  get '/sign-out', to: 'sessions#sign_out', as: 'sign_out'
-
   get '/auth', to: 'modders#auth', as: 'auth'
   get '/onboarding', to: 'modders#onboarding', as: 'onboarding'
   get '/profile', to: 'modders#profile', as: 'profile'
