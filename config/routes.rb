91fa5b023537b593c3aef856be0b730008ad0de2
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-17 23:46:17 -0500

    footer

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/ping', to: 'application#ping'
 
   get '/about', to: 'application#about'
+  get '/terms', to: 'application#terms'
 
   get '/profile', to: 'profiles#show', as: :user_root
   resource :profile, only: [:edit, :update, :create]
