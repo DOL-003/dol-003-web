ff54be59ced6661788885427a384cb1ae213a04e
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-18 00:35:31 -0500

    user menu

--- app/frontend/entrypoints/base.ts
+++ app/frontend/entrypoints/base.ts
@@ -1,1 +1,10 @@
-console.log("BASE")
+window.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.querySelector("#user-menu-toggle")
+  if (toggle) {
+    document.addEventListener("click", event => {
+      if (toggle.checked && !event.target.closest(".user-menu")) {
+        toggle.checked = false
+      }
+    })
+  }
+})
