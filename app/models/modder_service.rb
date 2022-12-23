55d78690f0aa06235764d55b056e051b871016ec
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-23 16:36:08 -0500

    service selector styling

--- app/models/modder_service.rb
+++ app/models/modder_service.rb
@@ -17,7 +17,21 @@ class ModderService < ApplicationRecord
 
   SERVICES = [
     { name: 'prebuilt controllers', color: '#fdb03a' },
-    { name: 'repairs', color: '#fdb03a' }
+    { name: 'diagnostics/repairs', color: '#fdb03a' },
+    { name: 'phobs', color: '#fdb03a' },
+    { name: 'snapback modules', color: '#fdb03a' },
+    { name: 'accepting send-ins', color: '#fdb03a' },
+    { name: 'commissions open', color: '#fdb03a' },
+    { name: 'resin buttons', color: '#fdb03a' },
+    { name: 'resin sticks', color: '#fdb03a' },
+    { name: 'resin triggers', color: '#fdb03a' },
+    { name: 'resin shells', color: '#fdb03a' },
+    { name: 'shell painting', color: '#fdb03a' },
+    { name: 'shell tinting', color: '#fdb03a' },
+    { name: 'shell dyeing', color: '#fdb03a' },
+    { name: 'electrical mods', color: '#fdb03a' },
+    { name: 'paracords', color: '#fdb03a' },
+    { name: 'notching', color: '#fdb03a' },
   ]
 
   belongs_to :modder
