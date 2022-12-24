382ba1bdd9a36a21b8e135dfb5ecfa4959e53389
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-24 18:35:51 -0500

    location selector stub

---
+++ app/frontend/components/LocationSelector.tsx
@@ -0,0 +1,13 @@
+import "./LocationSelector.scss"
+
+import React from "react"
+
+interface LocationSelectorProps {
+
+}
+
+export default (props: LocationSelectorProps) => {
+  return (
+    <div>hi</div>
+  )
+}
