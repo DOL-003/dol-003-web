0ff14cc0d8fae04a30af2df1c243350637e3d0a4
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-29 17:28:28 -0500

    photo manager base

--- app/frontend/components/PhotoManager.tsx
+++ app/frontend/components/PhotoManager.tsx
@@ -1,7 +1,36 @@
 import "./PhotoManager.scss"
 
 import React, { useState } from "react"
+import classNames from "classnames"
 
 export default (props) => {
-  return <div>hi</div>
+  const [open, setOpen] = useState(false)
+
+  function handleOpenToggleClick() {
+    setOpen(!open)
+  }
+
+  return (
+    <div className={classNames("PhotoManager", { open })}>
+      {(() => {
+        if (open)
+          return (
+            <>
+              <div className="overlay"></div>
+              <div className="manager">i'm managing dude</div>
+            </>
+          )
+        else
+          return (
+            <button
+              type="button"
+              className="button secondary"
+              onClick={handleOpenToggleClick}
+            >
+              Manage photos
+            </button>
+          )
+      })()}
+    </div>
+  )
 }
