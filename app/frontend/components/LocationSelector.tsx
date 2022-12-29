d598dabe9a950018a2157417b832b40261a47af9
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-28 21:42:45 -0500

    index location search

--- app/frontend/components/LocationSelector.tsx
+++ app/frontend/components/LocationSelector.tsx
@@ -1,13 +1,16 @@
 import "./LocationSelector.scss"
 
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import Autocomplete from "react-google-autocomplete"
 
 interface LocationSelectorProps {
   readonly modelName: string
-  readonly city: string
-  readonly latitude: string
-  readonly longitude: string
+  readonly city?: string
+  readonly latitude?: string
+  readonly longitude?: string
+  readonly class?: string
+  readonly placeholder?: string
+  readonly autosubmit?: boolean
 }
 
 interface PlaceResult {
@@ -24,22 +27,26 @@ export default (props: LocationSelectorProps) => {
   const [city, setCity] = useState(props.city || "")
   const [latitude, setLatitude] = useState(props.latitude || "")
   const [longitude, setLongitude] = useState(props.longitude || "")
+  const input = useRef()
 
   const handlePlaceSelected = (place: PlaceResult) => {
     setCity(place.formatted_address)
     setLatitude(place.geometry.location.lat().toString())
     setLongitude(place.geometry.location.lng().toString())
+    if (props.autosubmit) input.current.form.submit()
   }
 
   return (
     <>
       <Autocomplete
-        className="LocationSelector text-input large"
+        className={`LocationSelector text-input large ${props.class || ""}`}
         apiKey="AIzaSyAj_d9mLSJyQkQYNobEfqTY95bMmI2YTps"
         onPlaceSelected={handlePlaceSelected}
         defaultValue={props.city}
         options={{ fields: ["geometry.location", "formatted_address"] }}
         onFocus={(event) => setTimeout(() => event.target.select(), 10)}
+        placeholder={props.placeholder || "Enter your location"}
+        ref={input}
       />
       <input type="hidden" name={`${props.modelName}[city]`} value={city} />
       <input
