ffd6802fe06463fe1722a484d4df3bfb13b78cb6
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-31 00:41:59 -0500

    map toggle

--- app/frontend/entrypoints/modders.ts
+++ app/frontend/entrypoints/modders.ts
@@ -1,9 +1,11 @@
 window.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#map-toggle").addEventListener("click", () => {
-    const mapInput: HTMLInputElement = document.querySelector("#map-input")
-    if (mapInput) mapInput.value = mapInput.value === "1" ? "0" : "1"
+  document.querySelectorAll(".map-toggle").forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+      const mapInput: HTMLInputElement = document.querySelector("#map-input")
+      if (mapInput) mapInput.value = mapInput.value === "1" ? "0" : "1"
 
-    const results = document.querySelector("#modder-search-results")
-    if (results) results.classList.toggle("map-visible")
+      const results = document.querySelector("#modder-search-results")
+      if (results) results.classList.toggle("map-visible")
+    })
   })
 })
