b440e7482c5e82d59ad40a6b48db3b568b1000a6
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-31 01:08:59 -0500

    zoom to modders

--- app/frontend/components/ModderMap.tsx
+++ app/frontend/components/ModderMap.tsx
@@ -1,6 +1,6 @@
 import "./ModderMap.scss"
 
-import React from "react"
+import React, { useRef } from "react"
 import Map, { Marker } from "react-map-gl"
 
 import PinIcon from "@/icons/map-pin.svg"
@@ -20,18 +20,54 @@ interface ModderMapProps {
 }
 
 export default (props: ModderMapProps) => {
+  const map = useRef()
+
+  function handleMapLoad() {
+    if (props.modders) {
+      let minLatitude, maxLatitude, minLongitude, maxLongitude
+
+      props.modders.forEach((modder) => {
+        if (!minLatitude || parseFloat(modder.latitude) < minLatitude)
+          minLatitude = parseFloat(modder.latitude)
+        if (!maxLatitude || parseFloat(modder.latitude) > maxLatitude)
+          maxLatitude = parseFloat(modder.latitude)
+        if (!minLongitude || parseFloat(modder.longitude) < minLongitude)
+          minLongitude = parseFloat(modder.longitude)
+        if (!maxLongitude || parseFloat(modder.longitude) > maxLongitude)
+          maxLongitude = parseFloat(modder.longitude)
+      })
+
+      const latitudePadding = (maxLatitude - minLatitude) * 0.1
+      const longitudePadding = (maxLongitude - minLongitude) * 0.1
+
+      if (map.current)
+        map.current.fitBounds([
+          {
+            lng: maxLongitude + longitudePadding,
+            lat: minLatitude - latitudePadding,
+          },
+          {
+            lng: minLongitude - longitudePadding,
+            lat: maxLatitude + latitudePadding,
+          },
+        ])
+    }
+  }
+
   return (
     <div className="ModderMap">
       <Map
         mapboxAccessToken="pk.eyJ1Ijoiam1hcnF1aXMiLCJhIjoiY2xjNWI5Z25vMGFiOTNvbDduMm1xdjd6OSJ9.sIvPPG3HemKrXvtW-fVqYg"
         initialViewState={{
-          longitude: parseFloat(props.longitude),
-          latitude: parseFloat(props.latitude),
+          longitude: parseFloat(props.longitude) || -94.5785667,
+          latitude: parseFloat(props.latitude) || 39.0997265,
           zoom: 7,
         }}
         interactive={props.interactive || false}
         style={{ width: "100%", height: "100%" }}
         mapStyle="mapbox://styles/mapbox/light-v11"
+        ref={map}
+        onLoad={handleMapLoad}
       >
         {(() => {
           if (props.modders)
