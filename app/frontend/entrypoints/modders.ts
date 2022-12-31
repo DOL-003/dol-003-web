0ce515977abae0bf82a558b70456600adc21d505
Jeremy Marquis <jeremy@jeremymarquis.com>
2022-12-31 00:45:15 -0500

    animation fix

--- app/frontend/entrypoints/modders.ts
+++ app/frontend/entrypoints/modders.ts
@@ -1,10 +1,12 @@
 window.addEventListener("DOMContentLoaded", () => {
+  const results = document.querySelector("#modder-search-results")
+  if (results) results.classList.toggle("initialized")
+
   document.querySelectorAll(".map-toggle").forEach((toggle) => {
     toggle.addEventListener("click", () => {
       const mapInput: HTMLInputElement = document.querySelector("#map-input")
       if (mapInput) mapInput.value = mapInput.value === "1" ? "0" : "1"
 
-      const results = document.querySelector("#modder-search-results")
       if (results) results.classList.toggle("map-visible")
     })
   })
