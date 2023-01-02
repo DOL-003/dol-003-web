0df2d51cd839e83fcb9c3108806f00cc87c59f10
Jeremy Marquis <jeremy@jeremymarquis.com>
2023-01-02 16:11:45 -0500

    sentry init

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,6 @@
+Sentry.init do |config|
+  config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  config.traces_sample_rate = 1.0
+end
