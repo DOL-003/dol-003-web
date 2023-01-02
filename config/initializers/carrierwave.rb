3e2189aebe34458d5ded60d9a90251056ab8a17f
Jeremy Marquis <jeremy@jeremymarquis.com>
2023-01-02 16:11:41 -0500

    fog fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key: Rails.application.credentials.dig(:spaces, :access_key),
+    aws_access_key_id: Rails.application.credentials.dig(:spaces, :access_key),
     aws_secret_access_key: Rails.application.credentials.dig(:spaces, :access_key_secret),
     region: 'nyc3',
     endpoint: 'https://nyc3.digitaloceanspaces.com'
