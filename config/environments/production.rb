a53919d807e6e9d9cc1fca8ac7340b72a135bd3a
Jeremy Marquis <jeremy@jeremymarquis.com>
2023-01-02 16:40:23 -0500

    sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: 'apikey',
+    password: Rails.application.credentials.dig(:sendgrid, :api_key),
+    domain: 'dol-003.info',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
