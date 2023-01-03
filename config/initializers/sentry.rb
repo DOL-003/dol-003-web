4272b7b166d2cd9a93370fd84a50113abd707500
Jeremy Marquis <jeremy@jeremymarquis.com>
2023-01-03 12:43:31 -0500

    disable sentry transactions

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,4 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.dig(:sentry, :dsn)
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-
-  config.traces_sample_rate = 1.0
 end
