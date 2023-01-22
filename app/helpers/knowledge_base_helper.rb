7021ae36c87a13409983c52abaa521b57eb8151c
Jeremy Marquis <jeremy@jeremymarquis.com>
2023-01-22 17:47:28 -0500

    kb menu update

--- app/helpers/knowledge_base_helper.rb
+++ app/helpers/knowledge_base_helper.rb
@@ -4,11 +4,15 @@ module KnowledgeBaseHelper
     tree = tree.collect
     render 'menu_tree', locals: {
       tree: tree.collect do |title, item|
-        {
-          title:,
-          path: item.is_a?(String) ? File.join(path, item) : File.join(path, item[:path]),
-          children: item.is_a?(Hash) ? item.except(:path) : nil
-        }
+        if item == nil
+          'separator'
+        else
+          {
+            title:,
+            path: item.is_a?(String) ? File.join(path, item) : File.join(path, item[:path]),
+            children: item.is_a?(Hash) ? item.except(:path) : nil
+          }
+        end
       end
     }
   end
