e5ea661eaccffb1f57d7eaf209bc94b64f9f0f4b
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-07-12 15:33:44 -0400

    r2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,17 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.dig(:spaces, :access_key),
-    aws_secret_access_key: Rails.application.credentials.dig(:spaces, :access_key_secret),
-    region: 'nyc3',
-    endpoint: 'https://nyc3.digitaloceanspaces.com'
+    aws_access_key_id: Rails.application.credentials.dig(:r2, :access_key),
+    aws_secret_access_key: Rails.application.credentials.dig(:r2, :access_key_secret),
+    region: 'auto',
+    endpoint: "https://#{Rails.application.credentials.dig(:r2, :api_account_id)}.r2.cloudflarestorage.com",
+    enable_signature_v4_streaming: false
   }
-  config.fog_directory  = 'controllers-photos'
+  config.fog_directory  = 'controllers'
   config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+
+  if Rails.env.production?
+    config.asset_host = 'https://assets.dol-003.info'
+  end
 
 end
