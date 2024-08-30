9e417a67923c7b46a98ef879356d28f7504de2de
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-08-29 21:37:08 -0400

    better sorting algorithm

--- app/frontend/components/QuickSearch.tsx
+++ app/frontend/components/QuickSearch.tsx
@@ -1,7 +1,8 @@
 import "./QuickSearch.scss"
 
 import React, { useState, useEffect, useRef } from "react"
-import Select, { ControlProps, OptionProps, components } from "react-select"
+import { ControlProps, OptionProps, components } from "react-select"
+import AsyncSelect from "react-select/async"
 import classNames from "classnames"
 
 import GccIcon from "@/icons/gcc.svg"
@@ -77,9 +78,7 @@ const Option = (props: OptionProps<Modder | Page>) => {
                 {!props.data.logoUrl && <GccIcon />}
               </figure>
               <div>
-                <h3>
-                  {props.data.name} blah blah blah blah blah blah blah blah
-                </h3>
+                <h3>{props.data.name}</h3>
                 <p className="city">
                   <PinIcon />
                   {props.data.city}
@@ -105,6 +104,45 @@ const Option = (props: OptionProps<Modder | Page>) => {
   )
 }
 
+const filterOptions = (options: (Modder | Page)[], inputValue: string) => {
+  if (!inputValue) return options
+
+  const optionsWithScores = options.reduce((optionsWithScores, option) => {
+    let score = 0
+
+    switch (option.type) {
+      case "modder":
+        if (option.name.toLowerCase().includes(inputValue.toLowerCase()))
+          score += 10
+        if (option.name.toLowerCase().indexOf(inputValue.toLowerCase()) === 0)
+          score += 100 - option.name.length
+        if (option.city.toLowerCase().includes(inputValue.toLowerCase()))
+          score += 5
+        break
+      case "page":
+        if (option.title.toLowerCase().includes(inputValue.toLowerCase()))
+          score += 10
+        if (option.title.toLowerCase().indexOf(inputValue.toLowerCase()) === 0)
+          score += 100 - option.title.length
+        if (option.subtitle.toLowerCase().includes(inputValue.toLowerCase()))
+          score += 5
+        break
+    }
+
+    return optionsWithScores.concat([
+      {
+        score,
+        option,
+      },
+    ])
+  }, [])
+
+  return optionsWithScores
+    .filter((optionWithScore) => optionWithScore.score > 0)
+    .sort((a, b) => b.score - a.score)
+    .map((optionWithScore) => optionWithScore.option)
+}
+
 export default (props: QuickSearchProps) => {
   const [open, setOpen] = useState(false)
   const field = useRef()
@@ -128,22 +166,31 @@ export default (props: QuickSearchProps) => {
     setOpen(open)
   }
 
+  const loadOptions = (inputValue: string) => {
+    const options = [
+      ...props.modders.map((modder) => ({
+        type: "modder",
+        value: `${modder.name} ${modder.city}`,
+        ...modder,
+      })),
+      ...props.pages.map((page) => ({
+        type: "page",
+        value: `${page.title} ${page.subtitle}`,
+        ...page,
+      })),
+    ]
+
+    return new Promise<(Modder | Page)[]>((resolve) => {
+      resolve(filterOptions(options, inputValue))
+    })
+  }
+
   return (
     <>
-      <Select
+      <AsyncSelect
         ref={field}
-        options={[
-          ...props.modders.map((modder) => ({
-            type: "modder",
-            value: `${modder.name} ${modder.city}`,
-            ...modder,
-          })),
-          ...props.pages.map((page) => ({
-            type: "page",
-            value: `${page.title} ${page.subtitle}`,
-            ...page,
-          })),
-        ]}
+        loadOptions={loadOptions}
+        defaultOptions
         unstyled={true}
         placeholder={
           open ? "Search modders and Compendium pages" : "Quick search"
