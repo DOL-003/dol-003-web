f184c2413a3d83bc5d826a408262f73fd72847c8
Jeremy Marquis <jeremy@jeremymarquis.com>
2024-09-18 22:42:55 -0400

    default keys

--- config/initializers/turnstile.rb
+++ config/initializers/turnstile.rb
@@ -1,6 +1,6 @@
 RailsCloudflareTurnstile.configure do |config|
-  config.site_key = Rails.application.credentials.dig(:turnstile, :site_key)
-  config.secret_key = Rails.application.credentials.dig(:turnstile, :secret_key)
+  config.site_key = Rails.application.credentials.dig(:turnstile, :site_key) || '1x00000000000000000000AA'
+  config.secret_key = Rails.application.credentials.dig(:turnstile, :secret_key) || '1x0000000000000000000000000000000AA'
   config.fail_open = true
   config.mock_enabled = false
 end
