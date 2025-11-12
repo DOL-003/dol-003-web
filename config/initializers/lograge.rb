d7c42c80471d7e457abb7a8bd497afca90232349
Jeremy Marquis <jeremy@jeremymarquis.com>
2025-11-11 22:48:54 -0500

    log ip in requests

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,3 +1,9 @@
 Rails.application.configure do
   config.lograge.enabled = true
+
+  config.lograge.custom_options = lambda do |event|
+    {
+      ip: event.payload[:request].remote_ip
+    }
+  end
 end
