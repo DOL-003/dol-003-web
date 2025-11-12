33bbea16bc8d6f47a0cda74ac30d0085e392e311
Jeremy Marquis <jmarquis@etsy.com>
2025-11-12 16:07:24 -0500

    fix

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -9,7 +9,7 @@ class BaseController < ActionController::Base
     if request.path.present?
       path_segments = request.path.split('/').filter(&:present?)
       path_segments = ['root'] if path_segments.empty?
-      StatsD.increment("request_path.#{path_segments.join('.')}")
+      StatsD.increment("request_path.#{path_segments.join('_')}")
     end
     StatsD.increment("request.#{controller_name}.#{action_name}")
     StatsD.measure("request.#{controller_name}.#{action_name}") do
