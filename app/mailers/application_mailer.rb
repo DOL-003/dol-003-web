7df54b252c794f9db65ac318a423c5cf290c5b0a
Jeremy Marquis <jmarquis@etsy.com>
2025-11-12 11:25:04 -0500

    email metrics

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,6 +5,7 @@ class ApplicationMailer < ActionMailer::Base
 
   def log_delivery
     Rails.logger.info "Sent email: #{mailer_name}##{action_name}"
+    StatsD::increment("email_sent.#{mailer_name}.#{action_name}")
     EventLog.log 'email_sent', {
       mailer: mailer_name,
       email_name: action_name,
