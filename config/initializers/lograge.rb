e0e0855ff8cae9811dfad7ac77d0cff220472c56
Jeremy Marquis <jeremy@jeremymarquis.com>
2025-11-12 23:08:48 -0500

    don't log health checks

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,8 +1,9 @@
 Rails.application.configure do
   config.lograge.enabled = true
-
+  config.lograge.ignore_actions = ['Rails::HealthController#show']
   config.lograge.custom_options = lambda do |event|
     {
+      time: Time.now.utc,
       ip: event.payload[:request].remote_ip
     }
   end
