695ca61e430de1072fbe82b3befd1b736c0c9eff
Jeremy Marquis <jmarquis@etsy.com>
2025-11-13 11:56:27 -0500

    fix 404 exceptions

--- app/controllers/exceptions_controller.rb
+++ app/controllers/exceptions_controller.rb
@@ -6,7 +6,7 @@ class ExceptionsController < ApplicationController
     @exception = request.env['action_dispatch.exception']
     @status_code = @exception.try(:status_code) || ActionDispatch::ExceptionWrapper.new(request.env, @exception).status_code
 
-    render view_for_code(@status_code), status: @status_code
+    render view_for_code(@status_code), status: @status_code, formats: :html
   end
 
   private
